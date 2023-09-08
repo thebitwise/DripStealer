78231014f0945b3703bdc9982fd446db784847a7
bitwise <bitwisethegreat@gmail.com>
2023-09-08 19:35:15 -0400

    improvements

--- DripStealer.lua
+++ DripStealer.lua
@@ -39,10 +39,9 @@ props[2] = 'Earwear'
 props[3] = 'Watch'
 props[4] = 'Bracelet'
 
+-- thanks to aaron for proper id's for components and props in stand context
 function get_stand_component_tbl(mdl_str)
     local _tbl = {}
-
-
     if mdl_str == 'mp_f_freemode_01' or  mdl_str == 'mp_m_freemode_01' then 
         _tbl[0] = 'Head'
         _tbl[1] = 'Mask'
@@ -80,24 +79,57 @@ end
 
 function get_stand_ped_model_name(mdl)
     local translated = mdl
-    pluto_switch mdl do 
-        case 'player_zero':
-            translated = 'Michael'
-            break
-        case 'player_one':
-            translated =  'Franklin'
-            break
-        case 'player_two':
-            translated = 'Trevor'
-            break
-        case 'mp_f_freemode_01':
-            translated = 'Online Female'
-            break
-        case 'mp_m_freemode_01':
-            translated = 'Online Male'
-            break
-    end
-    return translated
+    local translations = {
+        player_zero = 'Michael',
+        player_one = 'Franklin',
+        player_two = 'Trevor',
+        mp_f_freemode_01 = 'Online Female',
+        mp_m_freemode_01 = 'Online Male',
+        cs_orleans = 'Bigfoot',
+        ig_orleans = 'Bigfoot 2',
+        a_c_boar = 'Boar',
+        a_c_boar_02 = 'Boar 2',
+        a_c_cat_01 = 'Cat',
+        a_c_chimp = 'Chimp',
+        a_c_chimp_02 = 'Chimp 2',
+        a_c_chop = 'Chop',
+        a_c_chop_02 = 'Chop 2',
+        a_c_cow = 'Cow',
+        a_c_coyote = 'Coyote',
+        a_c_coyote_02 = 'Coyote 2',
+        a_c_deer = 'Deer',
+        a_c_deer_02 = 'Deer 2',
+        a_c_shepherd = 'German Shepherd',
+        a_c_hen = 'Hen',
+        a_c_husky = 'Husky',
+        a_c_mtlion = 'Mountain Lion',
+        a_c_mtlion_02 = 'Mountain Lion 2',
+        a_c_panther = 'Panther',
+        a_c_pig = 'Pig',
+        a_c_poodle = 'Poodle',
+        a_c_pug = 'Pug',
+        a_c_pug_02 = 'Pug 2',
+        a_c_rabbit = 'Rabbit',
+        a_c_rabbit_02 = 'Rabbit 2',
+        a_c_rat = 'Rat',
+        a_c_retriever = 'Golden Retriever',
+        a_c_rhesus = 'Rhesus',
+        a_c_rottweiler = 'Rottweiler',
+        a_c_westy = 'Westy',
+        a_c_dolphin = 'Dolphin',
+        a_c_fish = 'Fish',
+        a_c_sharkhammer = 'Hammer Shark',
+        a_c_humpback = 'Humpback',
+        a_c_killerwhale = 'Killer Whale',
+        a_c_stingray = 'Stingray',
+        a_c_sharktiger = 'Tiger Shark',
+        a_c_cormorant = 'Cormorant',
+        a_c_chickenhawk = 'Chicken Hawk',
+        a_c_crow = 'Crow', 
+        a_c_pigeon = 'Pigeon',
+        a_c_seagull = 'Seagull'
+    } 
+    return translations[mdl]
 end
 
 
